f5d67ea4ab525a035130f49847c36a71d74cbde2
Ubuntu <ubuntu@ip-172-31-37-192.ec2.internal>
2023-03-09 17:51:17 +0000

    “cambios”

--- test_fun.py
+++ test_fun.py
@@ -1,3 +1,19 @@
 """
 Your module description
 """
+
+import pytest
+from unittest.mock import Mock
+from dw_fr import dw_fr
+import json
+
+def test_traer(mocker):
+    mock_traer_datos = mocker.patch('traer.traer')
+    mock_traer_datos.return_value = '{"hits":{"hits":""}}'
+    cantidad = 3
+    resultado = traer(cantidad)
+    res_dic = json.loads(resultado)
+    assert isinstance(resultado, str)
+    assert resultado[0] == '{'
+    assert "hits" in res_dic.keys()
+    assert len(res_dic["hits"]["hits"]) >= cantidad
